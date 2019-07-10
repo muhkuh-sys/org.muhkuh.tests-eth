97280f8d020bd4d61b17e3931a38c84fd51ab57f
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-07-10 09:15:04 +0200

    Add a PHY macro for the netX90 (full).

--- eth/src/driver/netx90/phy.h
+++ eth/src/driver/netx90/phy.h
@@ -31,6 +31,59 @@ typedef enum PHY_SETUP_COMMAND_ENUM
 #define REG_BANK_BIST  3
 #define REG_BANK_VMDAC 7
 
+/* Default bank */
+#define REG_BASIC_CTRL                       0
+
+/* DSP bank */
+#define REG_DCBLW                            0
+#define REG_DBGCNTL                          1
+#define REG_AMP_THRESH_1                     2
+#define REG_AMP_THRESH_2                     3
+#define REG_AMP_THRESH_3                     4
+#define REG_AMP_THRESH_4                     5
+#define REG_AMP_THRESH_5                     6
+#define REG_AMP_THRESH_6                     7
+#define REG_AMP_THRESH_7                     8
+#define REG_ON_ENERGY_LEVEL                  9
+#define REG_OFF_ENERGY_LEVEL_0              10
+#define REG_OFF_ENERGY_LEVEL_1              11
+#define REG_ENERGY_CNT_THRSH                12
+#define REG_BINIT_0123                      13
+#define REG_BINIT_4567                      14
+#define REG_DP_BASE_QTH                     15
+#define REG_DIFFZ                           16
+#define REG_CONFIG                          17
+#define REG_A1_CONFIG                       18
+#define REG_A2_CONFIG                       19
+#define REG_A3_CONFIG                       20
+#define REG_A4_CONFIG                       21
+#define REG_A5_CONFIG                       22
+#define REG_A6_CONFIG                       23
+#define REG_A7_CONFIG                       24
+#define REG_CONFIG2                         25
+#define REG_A8_CONFIG                       26
+#define REG_A9_CONFIG                       27
+#define REG_A10_CONFIG                      28
+#define REG_A11_CONFIG                      29
+#define REG_GAIN1                           30
+#define REG_GAIN2                           31
+
+
+/* VMDAC bank */
+#define REG_ZQ_CAL_CONTROL                   0
+#define REG_ZQ_CAL_STATUS                    1
+#define REG_VM_DAC_CONTROL0                  2
+#define REG_VM_DAC_CONTROL1                  3
+#define REG_MISC_PHY_CONTROL0                4
+#define REG_MISC_PCS_CONTROL0                5
+#define REG_MISC_PCS_CONTROL1                6
+#define REG_MISC_STATUS                      7
+#define REG_MISC_STATUS1                     8
+#define REG_TXDAC_CODE_01                    9
+#define REG_TXDAC_CODE_23                   10
+#define REG_TXDAC_CODE_45                   11
+#define REG_TXDAC_CODE_6                    12
+
 
 #define PHY_SETUP_DW(a) ((a)&0xffU), (((a)>>8U)&0xffU), (((a)>>16U)&0xffU), (((a)>>24U)&0xffU)
 
@@ -70,10 +123,15 @@ typedef enum PHY_SETUP_COMMAND_ENUM
 
 
 
-#define PHY_SETUP_SYS_PHY_WRITE_BANK(PHY, BANK, REG, VAL) \
+#define PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(PHY, BANK, REG, VAL) \
 	PHY_SETUP_SYS_PHY_WRITE((PHY), 23, (VAL)), \
-	PHY_SETUP_SYS_PHY_WRITE((PHY), 20, (0x4400 | ((BANK) << 11) | (REG)))
+	PHY_SETUP_SYS_PHY_WRITE((PHY), 20, (0x4400 | ((BANK) << 11) | (REG))), \
+	PHY_SETUP_SYS_PHY_WRITE((PHY), 20, 0x0000)
 
+#define PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(PHY, BANK, REG, VAL) \
+	PHY_SETUP_SYS_PHY_WRITE((PHY), 23, (VAL)), \
+	PHY_SETUP_SYS_PHY_WRITE((PHY), 20, (0x4000 | ((BANK) << 11) | (REG))), \
+	PHY_SETUP_SYS_PHY_WRITE((PHY), 20, 0x0000)
 
 
 #define PHY_SETUP_DELAY(DELAY) \
--- eth/src/options_default.c
+++ eth/src/options_default.c
@@ -68,7 +68,7 @@ const ROMLOADER_OPTIONS_T t_default_options =
 		                HOSTMSK(phy_control_phy1_automdixen) |
 		                HOSTMSK(phy_control_phy0_enable) |
 		                HOSTMSK(phy_control_phy1_enable)
-#elif ASIC_TYP==ASIC_TYP_NETX90_MPW || ASIC_TYP==ASIC_TYP_NETX90
+#elif ASIC_TYP==ASIC_TYP_NETX90_MPW
 		.ulPhyControl = 0,
 		.tPhyMacroIntern =
 		{
@@ -114,8 +114,8 @@ const ROMLOADER_OPTIONS_T t_default_options =
 				/* sys_phy_write_bank(uiPhy, REG_BANK_BIST, 0x1c, 0x0000);
 				 * sys_phy_write_bank(uiPhy, REG_BANK_BIST, 0x1d, 0x0280);
 				 */
-				PHY_SETUP_SYS_PHY_WRITE_BANK(0U, REG_BANK_BIST, 0x1cU, 0x0000U),
-				PHY_SETUP_SYS_PHY_WRITE_BANK(0U, REG_BANK_BIST, 0x1dU, 0x0280U),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(0U, REG_BANK_BIST, 0x1cU, 0x0000U),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(0U, REG_BANK_BIST, 0x1dU, 0x0280U),
 
 				PHY_SETUP_DELAY(2),
 
@@ -124,14 +124,13 @@ const ROMLOADER_OPTIONS_T t_default_options =
 				 * sys_phy_write_bank(uiPhy, REG_BANK_VMDAC, 0x03, 0x0004);
 				 */
 				PHY_SETUP_SYS_PHY_WRITE(0U, 17U, 0x0040U),
-				PHY_SETUP_SYS_PHY_WRITE_BANK(0U, REG_BANK_VMDAC, 0x02U, 0xF07BU),
-				PHY_SETUP_SYS_PHY_WRITE_BANK(0U, REG_BANK_VMDAC, 0x03U, 0x0004U),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(0U, REG_BANK_VMDAC, 0x02U, 0xF07BU),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(0U, REG_BANK_VMDAC, 0x03U, 0x0004U),
 
 				/* restart auto-neg
 				 * sys_phy_write(uiPhy, 0, 0x1200);
 				 */
 				PHY_SETUP_SYS_PHY_WRITE(0U, 0U, 0x1200U),
-
 
 
 				/* uiPhy = 1; */
@@ -149,8 +148,8 @@ const ROMLOADER_OPTIONS_T t_default_options =
 				/* sys_phy_write_bank(uiPhy, REG_BANK_BIST, 0x1c, 0x0000);
 				 * sys_phy_write_bank(uiPhy, REG_BANK_BIST, 0x1d, 0x0280);
 				 */
-				PHY_SETUP_SYS_PHY_WRITE_BANK(1U, REG_BANK_BIST, 0x1cU, 0x0000U),
-				PHY_SETUP_SYS_PHY_WRITE_BANK(1U, REG_BANK_BIST, 0x1dU, 0x0280U),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(1U, REG_BANK_BIST, 0x1cU, 0x0000U),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(1U, REG_BANK_BIST, 0x1dU, 0x0280U),
 
 				PHY_SETUP_DELAY(2),
 
@@ -159,13 +158,223 @@ const ROMLOADER_OPTIONS_T t_default_options =
 				 * sys_phy_write_bank(uiPhy, REG_BANK_VMDAC, 0x03, 0x0004);
 				 */
 				PHY_SETUP_SYS_PHY_WRITE(1U, 17U, 0x0040U),
-				PHY_SETUP_SYS_PHY_WRITE_BANK(1U, REG_BANK_VMDAC, 0x02U, 0xF07BU),
-				PHY_SETUP_SYS_PHY_WRITE_BANK(1U, REG_BANK_VMDAC, 0x03U, 0x0004U),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(1U, REG_BANK_VMDAC, 0x02U, 0xF07BU),
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(1U, REG_BANK_VMDAC, 0x03U, 0x0004U),
 
 				/* restart auto-neg
 				 * sys_phy_write(uiPhy, 0, 0x1200);
 				 */
 				PHY_SETUP_SYS_PHY_WRITE(1U, 0U, 0x1200U)
+			}
+		},
+		.tPhyMacroExtern =
+		{
+			.aucMacro =
+			{
+				PHY_SETUP_COMMAND_End
+			}
+		}
+#elif ASIC_TYP==ASIC_TYP_NETX90
+		.ulPhyControl = 0,
+		.tPhyMacroIntern =
+		{
+			.aucMacro =
+			{
+#if 0
+
+				/******************************************************************
+				 *
+				 * Reset the INTPHY.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 1U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(10),
+#       if 0
+				/******************************************************************
+				 *
+				 * Set the configuration while reset is active.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 7U, 0U, 1U, 0U, 1U, 7U, 0U, 1U, 0U, 1U, 1U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(2),
+#       else
+				/******************************************************************
+				 *
+				 * Set the configuration while reset is active.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 7U, 0U, 1U, 0U, 1U, 0U, 0U, 0U, 0U, 0U, 1U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(2),
+#       endif
+				/* Port 0 */
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_ctrl_ext0/sizeof(unsigned long), PHY_SETUP_DW(0xf8000800U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_gain_table0/sizeof(unsigned long), PHY_SETUP_DW(0xcba98321U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_1/sizeof(unsigned long), PHY_SETUP_DW(1100),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_2/sizeof(unsigned long), PHY_SETUP_DW(900),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_3/sizeof(unsigned long), PHY_SETUP_DW(750),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_4/sizeof(unsigned long), PHY_SETUP_DW(600),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_5/sizeof(unsigned long), PHY_SETUP_DW(520),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_6/sizeof(unsigned long), PHY_SETUP_DW(450),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_7/sizeof(unsigned long), PHY_SETUP_DW(400),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_off_energy_level0/sizeof(unsigned long), PHY_SETUP_DW(0x87766558U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_binit0/sizeof(unsigned long), PHY_SETUP_DW(0x0eca8876U),
+#       if 0
+				/* Port 1 */
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_ctrl_ext1/sizeof(unsigned long), PHY_SETUP_DW(0xf8000800U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_gain_table1/sizeof(unsigned long), PHY_SETUP_DW(0xcba98321U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_1/sizeof(unsigned long), PHY_SETUP_DW(1100),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_2/sizeof(unsigned long), PHY_SETUP_DW(900),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_3/sizeof(unsigned long), PHY_SETUP_DW(750),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_4/sizeof(unsigned long), PHY_SETUP_DW(600),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_5/sizeof(unsigned long), PHY_SETUP_DW(520),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_6/sizeof(unsigned long), PHY_SETUP_DW(450),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_7/sizeof(unsigned long), PHY_SETUP_DW(400),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_off_energy_level1/sizeof(unsigned long), PHY_SETUP_DW(0x87766558U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_binit1/sizeof(unsigned long), PHY_SETUP_DW(0x0eca8876U),
+#       endif
+
+#       if 0
+				/******************************************************************
+				 *
+				 * Remove the reset.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 7U, 0U, 1U, 0U, 1U, 7U, 0U, 1U, 0U, 1U, 0U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(2),
+#       else
+				/******************************************************************
+				 *
+				 * Remove the reset.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 7U, 0U, 1U, 0U, 1U, 0U, 0U, 0U, 0U, 0U, 0U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(2),
+#       endif
+				/* PHY Fixup. */
+				/* DCBLW (Direct Current for Base Line Wander Canceler) */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_DCBLW, 0x1674U),
+				/* CONFIG2 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_CONFIG2, 0x10d0U),
+				/* A3_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_A3_CONFIG, 0x7500U),
+				/* A5_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_A5_CONFIG, 0x840eU),
+				/* A11_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_A11_CONFIG, 0x5400U),
+				/* MISC_PHY_CONTROL0: set tx_vd_amp_itrim = 0 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(0, REG_BANK_VMDAC, REG_MISC_PHY_CONTROL0, 0x0a08U),
+#       if 0
+				/* DCBLW (Direct Current for Base Line Wander Canceler) */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_DCBLW, 0x1674U),
+				/* CONFIG2 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_CONFIG2, 0x10d0U),
+				/* A3_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_A3_CONFIG, 0x7500U),
+				/* A5_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_A5_CONFIG, 0x840eU),
+				/* A11_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_A11_CONFIG, 0x5400U),
+				/* MISC_PHY_CONTROL0: set tx_vd_amp_itrim = 0 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(1, REG_BANK_VMDAC, REG_MISC_PHY_CONTROL0, 0x0a08U),
+#       endif
+				/* Restart AutoNeg (due to we reset PHY in AutoNeg mode) */
+				PHY_SETUP_SYS_PHY_WRITE(0U, 0U, 0x1200U),
+#       if 0
+				PHY_SETUP_SYS_PHY_WRITE(1U, 0U, 0x1200U),
+#       endif
+
+#else
+
+				/******************************************************************
+				 *
+				 * Reset the INTPHY.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 1U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(100),
+
+				/* Port 0 */
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_ctrl_ext0/sizeof(unsigned long), PHY_SETUP_DW(0xf8000800U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_gain_table0/sizeof(unsigned long), PHY_SETUP_DW(0xcba98321U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_1/sizeof(unsigned long), PHY_SETUP_DW(1000),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_2/sizeof(unsigned long), PHY_SETUP_DW(750),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_3/sizeof(unsigned long), PHY_SETUP_DW(610),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_4/sizeof(unsigned long), PHY_SETUP_DW(570),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_5/sizeof(unsigned long), PHY_SETUP_DW(535),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_6/sizeof(unsigned long), PHY_SETUP_DW(460),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh0_7/sizeof(unsigned long), PHY_SETUP_DW(407),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_off_energy_level0/sizeof(unsigned long), PHY_SETUP_DW(0x87766558U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_binit0/sizeof(unsigned long), PHY_SETUP_DW(0x0eca8876U),
+
+				/* Port 1 */
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_ctrl_ext1/sizeof(unsigned long), PHY_SETUP_DW(0xf8000800U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_gain_table1/sizeof(unsigned long), PHY_SETUP_DW(0xcba98321U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_1/sizeof(unsigned long), PHY_SETUP_DW(1000),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_2/sizeof(unsigned long), PHY_SETUP_DW(750),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_3/sizeof(unsigned long), PHY_SETUP_DW(610),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_4/sizeof(unsigned long), PHY_SETUP_DW(570),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_5/sizeof(unsigned long), PHY_SETUP_DW(535),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_6/sizeof(unsigned long), PHY_SETUP_DW(460),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_amp_thresh1_7/sizeof(unsigned long), PHY_SETUP_DW(407),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_off_energy_level1/sizeof(unsigned long), PHY_SETUP_DW(0x87766558U),
+				PHY_SETUP_COMMAND_WriteIntPhyCfg, REL_Adr_NX90_int_phy_cfg_phy_binit1/sizeof(unsigned long), PHY_SETUP_DW(0x0eca8876U),
+
+				/******************************************************************
+				 *
+				 * Set the configuration while reset is active.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 7U, 0U, 1U, 0U, 1U, 7U, 0U, 1U, 0U, 1U, 1U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(100),
+
+				/******************************************************************
+				 *
+				 * Remove the reset.
+				 *
+				 ******************************************************************/
+				PHY_SETUP_INTPHY_SetPhyCtrl(0U, 7U, 0U, 1U, 0U, 1U, 7U, 0U, 1U, 0U, 1U, 0U),
+				/* Delay for at least 1ms. */
+				PHY_SETUP_DELAY(100),
+
+				/* PHY Fixup. */
+				/* DCBLW (Direct Current for Base Line Wander Canceler) */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_DCBLW, 0x1674U),
+				/* CONFIG2 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_CONFIG2, 0x10d0U),
+				/* A3_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_A3_CONFIG, 0x7500U),
+				/* A5_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_A5_CONFIG, 0x840eU),
+				/* A11_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(0, REG_BANK_DSP, REG_A11_CONFIG, 0x5400U),
+				/* MISC_PHY_CONTROL0: set tx_vd_amp_itrim = 0 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(0, REG_BANK_VMDAC, REG_MISC_PHY_CONTROL0, 0x0a08U),
+
+				/* DCBLW (Direct Current for Base Line Wander Canceler) */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_DCBLW, 0x1674U),
+				/* CONFIG2 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_CONFIG2, 0x10d0U),
+				/* A3_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_A3_CONFIG, 0x7500U),
+				/* A5_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_A5_CONFIG, 0x840eU),
+				/* A11_CONFIG */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_DSP(1, REG_BANK_DSP, REG_A11_CONFIG, 0x5400U),
+				/* MISC_PHY_CONTROL0: set tx_vd_amp_itrim = 0 */
+				PHY_SETUP_SYS_PHY_WRITE_BANK_NOTDSP(1, REG_BANK_VMDAC, REG_MISC_PHY_CONTROL0, 0x0a08U),
+
+				/* Restart AutoNeg (due to we reset PHY in AutoNeg mode) */
+				PHY_SETUP_SYS_PHY_WRITE(0U, 0U, 0x1200U),
+				PHY_SETUP_SYS_PHY_WRITE(1U, 0U, 0x1200U),
+
+#endif
 			}
 		},
 		.tPhyMacroExtern =

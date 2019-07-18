bbc1dcd8ee28833e769ce6eaa17685e51ecbf17a
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-07-18 13:05:34 +0200

    Disable all ports by default.

--- eth/src/options_default.c
+++ eth/src/options_default.c
@@ -30,35 +30,35 @@ const ROMLOADER_OPTIONS_T t_default_options =
 			/* Port 0 */
 			{
 				.acName = "CH0",
-				.tInterface = INTERFACE_INTPHY0,
-				.tFunction = INTERFACE_FUNCTION_EchoClient,
+				.ulInterface = INTERFACE_None,
+				.ulFunction = INTERFACE_FUNCTION_None,
 				.ulFlags = 0,
-				.aucMac = { 0x00U, 0x02U, 0xa2U, 0x20U, 0x20U, 0x00U },
-				.ulIp = IP_ADR(192,168,64,20),
+				.ulIp = IP_ADR(0,0,0,0),
 				.ulGatewayIp = 0,
-				.ulNetmask = IP_ADR(255,255,255,0),
+				.ulNetmask = IP_ADR(0,0,0,0),
+				.ulRemoteIp = IP_ADR(0,0,0,0),
 				.usLinkUpDelay = 1000,
-				.usLocalPort = MUS2NUS(1024),
-				.ulRemoteIp = IP_ADR(192,168,64,1),
-				.usRemotePort = MUS2NUS(5555)
+				.usLocalPort = 0,
+				.usRemotePort = 0,
+				.aucMac = { 0x00U, 0x02U, 0xa2U, 0x20U, 0x20U, 0x00U }
 			},
 			/* Port 1 */
 			{
 				.acName = "CH1",
-				.tInterface = INTERFACE_INTPHY1,
-				.tFunction = INTERFACE_FUNCTION_EchoClient,
+				.ulInterface = INTERFACE_None,
+				.ulFunction = INTERFACE_FUNCTION_None,
 				.ulFlags = 0,
-				.aucMac = { 0x00U, 0x02U, 0xa2U, 0x20U, 0x20U, 0x01U },
-				.ulIp = IP_ADR(192,168,65,20),
+				.ulIp = IP_ADR(0,0,0,0),
 				.ulGatewayIp = 0,
 				.ulNetmask = IP_ADR(255,255,255,0),
 				.usLinkUpDelay = 1000,
-				.usLocalPort = MUS2NUS(1025),
-				.ulRemoteIp = IP_ADR(192,168,65,1),
-				.usRemotePort = MUS2NUS(5555)
+				.usLocalPort = 1025,
+				.ulRemoteIp = IP_ADR(192,168,64,20),
+				.usRemotePort = 1024,
+				.aucMac = { 0x00U, 0x02U, 0xa2U, 0x20U, 0x20U, 0x01U }
 			}
 		},
-		.usArpTimeout = 1000,
+		.usArpTimeout = 100,
 		.usDhcpTimeout = 3000,
 		.ucArpRetries = 10,
 		.ucDhcpRetries = 5,
